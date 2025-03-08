869b132a31070f30abc660a0c78b475d511deaf5
user <user@DESKTOP-L6LG0CR>
2025-03-08 15:23:48 -0800

    Add project files.

---
+++ AuraHospital/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace AuraHospital
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ AuraHospital/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AuraHospital;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
